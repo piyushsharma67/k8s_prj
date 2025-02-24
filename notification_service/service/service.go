5a5e82be2df3a73d0c2eeb78c53cf650aaeec2af
piyushsharma67 <piyush67.sharma@gmail.com>
2025-02-24 15:38:56 +0530

    added

--- notification_service/service/service.go
+++ notification_service/service/service.go
@@ -1,1 +1,13 @@
 package service
+
+import "notification_service/repository"
+
+type ServiceStruct struct{
+	repository repository.Repository
+}
+
+func InitialiseService(repository repository.Repository)*ServiceStruct{
+	return &ServiceStruct{
+		repository: repository,
+	}
+}
