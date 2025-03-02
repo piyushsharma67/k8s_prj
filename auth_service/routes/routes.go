bf52299f3973a549acbd00c7b2714efb203f9c9c
piyushsharma67 <piyush67.sharma@gmail.com>
2025-03-02 23:40:21 +0530

    working on auth

--- auth_service/routes/routes.go
+++ auth_service/routes/routes.go
@@ -29,9 +29,9 @@ func InitRoutes(service *services.ServiceStruct) *mux.Router {
 	c := contoller.NewHTTPController(service)
 
 	r.HandleFunc("/", c.Health)
-	r.HandleFunc("/v1/signup", c.SignupHttp).Methods("POST")
+	r.HandleFunc("signup", c.SignupHttp).Methods("POST")
 	r.HandleFunc("/login", c.LoginHttp).Methods("GET")
-	r.HandleFunc("/v1/save_fc_token", Protected(c.SaveUserFcmHttp)).Methods("POST")
+	r.HandleFunc("save_fc_token", Protected(c.SaveUserFcmHttp)).Methods("POST")
 
 	return r
 }
